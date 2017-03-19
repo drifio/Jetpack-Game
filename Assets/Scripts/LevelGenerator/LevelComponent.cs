ae8440ac0b9ae913b27473c86fd36f4718353fbe
drifio <drif@drif.io>
2017-03-19 00:53:04 +0000

    First draft of Random Level Generator

---
+++ Assets/Scripts/LevelGenerator/LevelComponent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+[ExecuteInEditMode]
+public class LevelComponent : MonoBehaviour
+{
+    public void Awake()
+    {
+
+    }
+
+    public void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/LevelGenerator/LevelSet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Level/Level Set", order = 1)]
+public class LevelSet : ScriptableObject
+{
+    public string Name = "";
+
+    public List<LevelComponent> SpawnComponent = new List<LevelComponent>();
+    public List<LevelComponent> CommonComponents = new List<LevelComponent>();
+    public List<LevelComponent> EndComponents = new List<LevelComponent>();
+}
