5b539e2852b16fa7716fa9293744559a5707b462
Jake Ratcliffe <JakeR1342@gmail.com>
2017-03-19 02:56:25 +0000

    Added comments for clarity.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,31 +10,34 @@ public class PlayerController : MonoBehaviour {
     public float jumpTime = 1f;
     public float maxJetVelocity = 1f;
 
-    private Rigidbody rb;
+    private Rigidbody rigidbody;
     private int fuel = 100;
     private bool grounded = true;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
+        // Check to see if we can jump, and if we are jumping
         if (grounded && Input.GetKeyDown(KeyCode.Space))
         {
             StartCoroutine(Jump());
         }
+
         Jetpack();
     }
 
     private void FixedUpdate()
     {
+        // Get user input for horizontal movement, turn that into a velocity, then apply it
         Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
         Vector3 velocity = input * speed * Time.fixedDeltaTime;
-
         transform.Translate(velocity);
 
+        // If we're on the ground, start refueling
         if (grounded)
         {
             fuel++;
@@ -43,6 +46,7 @@ public class PlayerController : MonoBehaviour {
 
     private void OnCollisionEnter(Collision collision)
     {
+        // If we're on the ground, we're grounded
         if (collision.collider.tag == "Ground")
         {
             grounded = true;
@@ -51,6 +55,7 @@ public class PlayerController : MonoBehaviour {
 
     private void OnCollisionExit(Collision collision)
     {
+        // If we leave the ground, we aren't grounded
         if (collision.collider.tag == "Ground")
         {
             grounded = false;
@@ -61,10 +66,12 @@ public class PlayerController : MonoBehaviour {
     {
         float time = 0f;
 
+        // Check if we're still trying to jump and if we've exceeded the jump time limit
         while (Input.GetKey(KeyCode.Space) && time <= jumpTime)
         {
+            // Apply the velocity, inverse lerped between 0 and the max jump time for smoother jumping
             float jumpLerp = 1 - Mathf.InverseLerp(0f, jumpTime, time);
-            rb.velocity = new Vector3(rb.velocity.x, jumpVelocity * jumpLerp, rb.velocity.z);
+            rigidbody.velocity = new Vector3(rigidbody.velocity.x, jumpVelocity * jumpLerp, rigidbody.velocity.z);
             time += Time.deltaTime;
             yield return null;
         }
@@ -72,14 +79,17 @@ public class PlayerController : MonoBehaviour {
 
     private void Jetpack()
     {
+        // Check to see if we are using the jetpack, and if we have enough fuel to keep using it
         if (Input.GetKey(KeyCode.W) && fuel > 0)
         {
+            // Burn fuel and accelerate upwards
             fuel--;
-            rb.AddForce(new Vector3(0f, jetpackForce, 0f) * Time.deltaTime);
+            rigidbody.AddForce(new Vector3(0f, jetpackForce, 0f) * Time.deltaTime);
 
-            if (rb.velocity.y > maxJetVelocity)
+            // Check to see if we've exceeded to that max vertical upwards velocity
+            if (rigidbody.velocity.y > maxJetVelocity)
             {
-                rb.velocity = new Vector3(rb.velocity.x, maxJetVelocity, rb.velocity.z);
+                rigidbody.velocity = new Vector3(rigidbody.velocity.x, maxJetVelocity, rigidbody.velocity.z);
             }
         }
     }
